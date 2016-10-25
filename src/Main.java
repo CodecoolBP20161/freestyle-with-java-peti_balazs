e52f1ff6d6ef81759ef0400ba0f74d6857908186
Csyk <balipaulcsik@gmail.com>
2016-10-25 11:38:20 +0200

    user input implemented

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,33 @@ import model.CurrencyInEur;
 import model.CurrencyInUsd;
 
 import java.math.BigDecimal;
+import java.util.Scanner;
 
 /**
  * Created by csyk on 2016.10.24..
  */
 public class Main {
     public static void main(String[] args) {
-        CurrencyInUsd currency1 = new CurrencyInUsd("CAD", "USD", BigDecimal.valueOf(0.749541));
-        CurrencyInEur currency2 = new CurrencyInEur("HUF", "EUR", BigDecimal.valueOf(0.00324589441));
-        Amount amount = new Amount(BigDecimal.valueOf(100));
 
-       Exchanger changer = new Exchanger();
+        Scanner reader = new Scanner(System.in);  // Reading from System.in
+
+        System.out.println("Enter a number: ");
+        Amount amount = new Amount(reader.nextBigDecimal()); // Scans the next token of the input as a BigDecimal.
+
+        System.out.println("Enter a currency: ");
+        String currencyName1 = reader.next();
+        System.out.println("Enter another currency: ");
+        String baseCurrency1 = reader.next();
+
+        System.out.println("Enter a currency: ");
+        String currencyName2 = reader.next();
+        System.out.println("Enter another currency: ");
+        String baseCurrency2 = reader.next();
+
+        CurrencyInUsd currency1 = new CurrencyInUsd(currencyName1, baseCurrency1, BigDecimal.valueOf(0.749541));
+        CurrencyInEur currency2 = new CurrencyInEur(currencyName2, baseCurrency2, BigDecimal.valueOf(0.00324589441));
+
+        Exchanger changer = new Exchanger();
 
         System.out.println(changer.exchanger(currency1, currency2, amount));
     }
